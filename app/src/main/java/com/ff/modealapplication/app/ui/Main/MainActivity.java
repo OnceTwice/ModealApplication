e840801c30d5a841fa67cd6e12de0e086f522cd1
motherfather <father@mail.com>
2017-01-23 19:37:44 +0900

    Merge branch 'master' of C:\LSW\Android\AndroidStudioProjects\ModealApplication with conflicts.

--- app/src/main/java/com/ff/modealapplication/app/ui/Main/MainActivity.java
+++ app/src/main/java/com/ff/modealapplication/app/ui/Main/MainActivity.java
@@ -1,12 +1,29 @@
 package com.ff.modealapplication.app.ui.Main;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 import android.widget.Toast;
 
 import com.ff.modealapplication.R;
@@ -17,20 +34,126 @@ import com.ff.modealapplication.app.core.service.MainService;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
-    private MainListArrayAdapter mainListArrayAdapter =null;
+    private MainListArrayAdapter mainListArrayAdapter = null;
+    AppCompatDelegate delegate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // ListActivity에서 setSupportActionBar를 쓰기 위해서 한것들... (170123/상욱추가)
+        // ListActivity를 상속 받지 않으면 이거 슬필요 없습니다
+        AppCompatCallback callback = new AppCompatCallback() {
+            @Override
+            public void onSupportActionModeStarted(ActionMode mode) {
+
+            }
+
+            @Override
+            public void onSupportActionModeFinished(ActionMode mode) {
+            }
+
+            @Nullable
+            @Override
+            public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
+                return null;
+            }
+
+        };
+        delegate = AppCompatDelegate.create(this, callback);
+        delegate.onCreate(savedInstanceState);
+        delegate.setContentView(R.layout.activity_main);
+
+        // 툴바 추가 (170123/상욱추가)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        delegate.setSupportActionBar(toolbar);
+        delegate.getSupportActionBar().setDisplayShowTitleEnabled(false); // 타이틀에 있는 앱이름 숨기기
+
+        // 네비게이션 추가(170123/상욱추가)
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // 로그인 버튼 클릭시 액티비티 이동!!!
+        ((NavigationView)findViewById(R.id.nav_view)).getHeaderView(0).findViewById(R.id.login_button).setOnClickListener(this);
+
 
         mainListArrayAdapter = new MainListArrayAdapter(this);
         setListAdapter(mainListArrayAdapter);
 
         new MainListAsyncTask().execute();
     }
+
+    // onClick 매번 생성 하기 없이 한꺼번에 하기 위해서 (170123/상욱추가)
+    // case 추가하면서 온클릭 사용하세요~
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button: {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+    // 단순히 액션바부터 네비게이션 돋보기 추가하기 위해서는 여기서 부터~~~
+    // 뒤로가기 버튼 누를때... (170123/상욱추가)
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // 액션바 맨 오른쪽 돋보기 추가 (170123/상욱추가)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        delegate.getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    // 돋보기 눌렀을때 효과... (170123/상욱추가)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 이곳에 돋보기 눌렀을때 검색액티비티로 이동할 코드 구현!!!
+        Toast.makeText(this, "오오오됐어~", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+    // 네비게이션 (170123/상욱추가)
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_myPage) {
+
+        } else if (id == R.id.nav_bookmark) {
+
+        } else if (id == R.id.nav_setup) {
+
+        } else if (id == R.id.nav_help) {
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    // 여기까지 복붙~~
 
     private class MainListAsyncTask extends SafeAsyncTask<List<Map<String, Object>>>{
         @Override
