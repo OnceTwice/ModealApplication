8e37557eef990b89971c1133d0e5d98b75c6698e
Your Name <you@example.com>
2017-01-31 11:12:30 +0900

    0131 logo 변경

--- app/src/main/java/com/ff/modealapplication/app/ui/Main/MainListArrayAdapter.java
+++ app/src/main/java/com/ff/modealapplication/app/ui/Main/MainListArrayAdapter.java
@@ -47,7 +47,7 @@ public class MainListArrayAdapter extends ArrayAdapter<Map<String, Object>> {
             .build();
 
     public MainListArrayAdapter(Context context) {
-        super(context, R.layout.activity_main);
+        super(context, R.layout.activity_main2);
         layoutInflater = LayoutInflater.from(context);
     }
 
@@ -63,7 +63,7 @@ public class MainListArrayAdapter extends ArrayAdapter<Map<String, Object>> {
 
         //이미지 저장 [ 연결된 ip로 upload ] 위치에 내용이 있어야 한다.
         ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(getContext()));
-        ImageLoader.getInstance().displayImage("http://192.168.1.15:8088/modeal/shop/images/"+itemVo.get("picture"), (ImageView)view.findViewById(R.id.mian_image_item), displayImageOptions);
+        ImageLoader.getInstance().displayImage("http://192.168.1.14:8888/modeal/shop/images/"+itemVo.get("picture"), (ImageView)view.findViewById(R.id.mian_image_item), displayImageOptions);
 
         //내용저장
         TextView textTimeView=(TextView)view.findViewById(R.id.main_time_textView);
