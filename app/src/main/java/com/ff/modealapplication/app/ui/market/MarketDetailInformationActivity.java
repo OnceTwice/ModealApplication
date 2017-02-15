bde85067cb4386d31a395b3a77690f6a02a60397
cho <you-smile@nate.com>
2017-02-15 19:39:17 +0900

    매장상세정보 레이아웃 대략 완성

--- app/src/main/java/com/ff/modealapplication/app/ui/market/MarketDetailInformationActivity.java
+++ app/src/main/java/com/ff/modealapplication/app/ui/market/MarketDetailInformationActivity.java
@@ -1,8 +1,9 @@
 package com.ff.modealapplication.app.ui.market;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ff.modealapplication.R;
 
@@ -12,7 +13,22 @@ public class MarketDetailInformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_detail_information);
-        Intent intent = new Intent(this.getIntent());
 
+        // 자체 제작 액션바
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_marketDetail); // 아이디인 툴바 로그인 부분은 내 아이디로 변경해줘야함
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 화살표 표시
+
+    }
+
+    // 뒤로가기 클릭시 & 돋보기 클릭시
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 뒤로가지 클릭시
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
